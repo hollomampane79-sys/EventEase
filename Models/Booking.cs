e80925c80dc13603bd267164c55cfbcad212957a
Hollo Mampane <ST10184131@myemeris.edu.za>
2026-04-15 13:59:12 +0200

    Added validation to prevent deleting venues with bookings

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,7 +1,7 @@
 ﻿namespace EventEase.Models
 {
 
-    //this is the booking models
+    //this is the booking models which will be used to create the booking table in the database and also to create the booking controller and views
     public class Booking
     {
         public int BookingId { get; set; }
